9975a1d21c37b89b27d53aca98d93b0188a3b4f5
M. Bianchi <marcusbianchi92@protonmail.com>
2022-09-04 20:40:21 -0300

    Primeiro

---
+++ routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+const app = require('../app');
+var router = express.Router();
+
+/* GET home page. */
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Marcus Bianchi Profile' });
+});
+
+router.get('/Profile', function(req, res){
+  res.render('Profile')
+});
+
+router.get('/Portfolio', function(req, res){
+  res.render('Portfolio')
+});
+
+router.get('/Education', function(req, res){
+  res.render('Education')
+});
+
+router.post('/add', function(req, res){
+});
+
+module.exports = router;
+
+
